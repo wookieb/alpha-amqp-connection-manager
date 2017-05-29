9ed86abfab32072734bcad9f2837ce39419b304d
Łukasz Kużyński <wookieb>
2017-05-29 06:26:42 +0200

    Proper exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import {default as ConnectionManager, ConnectionManagerOptions} from "./ConnectionManager";
-export * from './ConnectionManager';
+export {default as ConnectionManager, ConnectionManagerOptions, ReconnectOptions} from './ConnectionManager';
 
 export async function connect(url: string, options?: ConnectionManagerOptions) {
     const manager = new ConnectionManager(url, options);
